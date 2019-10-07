5efbe14725ce1f3a781c1e595c3e21ca77f2dfa1
ruhomor <ruhomor@icloud.com>
2019-10-07 16:16:13 +0300

    a

--- C_fail.c
+++ C_fail.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
 
-int	pow2(int s)
-{
-	int	res;
-
-	res = 1;
-	while (s)
-	{
-		res *= 2;
-		s--;
-	}
-	return (res);
-}
-
 int	main()
 {
-	printf("%d", pow2(sizeof(unsigned long long int)));
+	printf("%lu", 8 * sizeof(unsigned long long int));
 	return (0);
 }
