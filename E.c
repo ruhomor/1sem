e1e8aa35b067d46396271946341730cc432614a3
ruhomor <ruhomor@icloud.com>
2019-10-09 13:12:15 +0300

    a

---
+++ E.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int	main()
+{
+	int dh;
+	int dm;
+	int ah;
+	int am;
+	int resh;
+	int resm;
+
+	scanf("%d:%d\n%d:%d", &dh, &dm, &ah, &am);
+	resh = ah + dh;
+	resm = am + dm;
+	if (resm >= 60)
+	{
+		resm -= 60;
+		resh++;
+	}
+	while (resh >= 24)
+		resh -= 24;
+	printf("%02d:%02d", resh, resm);
+	return (0);
+}
